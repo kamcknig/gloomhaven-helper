4142051cc71ebbe689683661cbb1263df2373124
Kyle McKnight <kamcknig@gmail.com>
2022-06-16 17:16:19 -0400

    added token section to the monster card.
    also updated monster cards to display based on scenario level

--- src/app/monster/components/active-monster-card/active-monster-card.component.ts
+++ src/app/monster/components/active-monster-card/active-monster-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MonsterInfo, MonsterService } from '../../services/monster.service';
+import { AppService } from '../../../app.service';
 
 @Component({
   selector: 'app-active-monster-card',
@@ -8,15 +9,47 @@ import { MonsterInfo, MonsterService } from '../../services/monster.service';
 })
 export class ActiveMonsterCard implements OnInit {
   @Input() value: MonsterInfo | undefined;
+  tokens: any[] = [{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  },{
+    number: 1,
+    health: 10
+  }];
 
   constructor(
-    public monsterService: MonsterService
+    public monsterService: MonsterService,
+    public appService: AppService
   ) { }
 
   ngOnInit(): void {
   }
 
   getStatDisplayValue(value: number | string | undefined) {
-    return value === 0 ? '-' : value;
+    return value || '-';
   }
 }
