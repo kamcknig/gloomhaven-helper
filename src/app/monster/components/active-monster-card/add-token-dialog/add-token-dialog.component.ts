dfaab27b3e685289522389f153b1cbd59c0520e8
Kyle McKnight <kyle.mcknight@accesso.com>
2022-06-17 13:43:26 -0400

    added comments

--- src/app/monster/components/active-monster-card/add-token-dialog/add-token-dialog.component.ts
+++ src/app/monster/components/active-monster-card/add-token-dialog/add-token-dialog.component.ts
@@ -10,21 +10,39 @@ import { TokenInfo } from '../../../../token/services/token.service';
 export class AddTokenDialogComponent implements OnInit {
   public value: { normals: TokenInfo[], elites: TokenInfo[] };
   public defaultNumbers: number[];
+
   constructor(
     private _dialogRef: DynamicDialogRef,
     private _dialogConfig: DynamicDialogConfig
   ) {
-    this.defaultNumbers = Array(10).fill(0).map((x, i) => i + 1);
+    this.defaultNumbers = Array(10)
+      .fill(0)
+      .map((x, i) => i + 1);
     this.value = this._dialogConfig.data;
   }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Checks if the token number is already taken and returns 'hidden' if it does, otherwise 'visible'
+   * @param index
+   */
   getTokenButtonVisible(index: number) {
-    return this.value?.elites?.concat(this.value?.normals ?? []).find(e => e.number === index) ? 'hidden' : 'visible' ?? 'visible';
+    return this.value?.elites?.concat(this.value?.normals ?? [])
+      ?.find(e => e.number === index)
+      ? 'hidden'
+      : 'visible'
+      ?? 'visible';
   }
 
+  /**
+   * Event handler when user selects a token. Closes the dialog and passes a Tuple with the number selected
+   * and whether it was an elite or not
+   *
+   * @param number
+   * @param elite
+   */
   selectTokenNumber(number: number, elite: boolean = false) {
     this._dialogRef.close([number, elite]);
   }
