7e1ffb2cb09d889e0526ef9f2eae5b55dd243abd
Kyle McKnight <kamcknig@gmail.com>
2022-06-19 10:48:52 -0400

    selection of a monster from the dialog now results in the correct monster being added to the active monster area

--- src/app/monster/activate-monster-dialog/activate-monster-dialog.component.ts
+++ src/app/monster/activate-monster-dialog/activate-monster-dialog.component.ts
@@ -9,9 +9,10 @@ import { DynamicDialogRef } from 'primeng/dynamicdialog';
   styleUrls: ['./activate-monster-dialog.component.scss']
 })
 export class ActivateMonsterDialogComponent implements OnInit {
+  selectedMonster: MonsterInfo | undefined;
   searchResults: MonsterInfo[] = [];
   selected: string | undefined;
-  availableMonsters: MonsterInfo[] | undefined;
+  availableMonsters: MonsterInfo[] = [];
 
   constructor(
     private _monsterService: MonsterService,
@@ -30,25 +31,37 @@ export class ActivateMonsterDialogComponent implements OnInit {
           }
           prev.push(next);
           return prev;
-        }, [] as MonsterInfo[]);
-      }).state$.subscribe({
-        next: value => {
-          console.log(value);
-          this.availableMonsters = value;
-        }
+        }, [] as MonsterInfo []);
       }
-    );
+    )
+      .state$
+      .subscribe({
+          next: value => {
+            console.log(value);
+            this.availableMonsters = value;
+          }
+        }
+      );
   }
 
   search($event: any) {
-    const query = $event.query as string;
+    let query = $event.query as string;
     if (!query) {
-      this.searchResults = this.availableMonsters!.concat();
+      this.searchResults = this.availableMonsters.concat();
       return;
     }
+
+    query = query.replace(/\s/g, '');
+    this.searchResults = this.availableMonsters.filter(
+      e => new RegExp(`${query}`, 'gi').test(e.name.replace(/\s/g, ''))
+    );
   }
 
   selectMonster($event: any) {
-    console.log($event);
+    this.selectedMonster = $event;
+  }
+
+  close(value: any) {
+    this._dialogRef.close(value);
   }
 }
