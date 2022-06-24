d54e599d1bcb35423fe9f2c71dea013ee435de85
Kyle McKnight <kyle.mcknight@accesso.com>
2022-06-24 16:36:54 -0400

    connected value of scenario level input to the store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { AppService } from './app.service';
 import { MonsterInfo, MonsterService } from './monster/services/monster.service';
-import { map, take } from 'rxjs';
+import { map, Subject, take, takeUntil } from 'rxjs';
 import { ActivateMonsterDialogComponent } from './monster/activate-monster-dialog/activate-monster-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { FormControl } from '@angular/forms';
@@ -15,6 +15,8 @@ export type AppMenuItem<T = any> = MenuItem & { metadata?: T };
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private _destroy$: Subject<void> = new Subject<void>();
+
   title = 'gloomhaven-helper';
   levelInputControl = new FormControl('');
 
@@ -35,26 +37,28 @@ export class AppComponent implements OnInit {
     this._dialogService.open(ActivateMonsterDialogComponent, {
       disableClose: false
       /*modal: true,
-      header: "Select a monster"*/
-    }).afterClosed().subscribe({
-      next: id => {
-        let monster: MonsterInfo | undefined;
-        this.monsterService.monsterStore.monsters$
-          .pipe(
-            map(monsters => monsters.find(m => m.id === id)),
-            take(1)
-          )
-          .subscribe({
-            next: (value) => monster = value
-          });
+       header: "Select a monster"*/
+    })
+      .afterClosed()
+      .subscribe({
+        next: id => {
+          let monster: MonsterInfo | undefined;
+          this.monsterService.monsterStore.monsters$
+            .pipe(
+              map(monsters => monsters.find(m => m.id === id)),
+              take(1)
+            )
+            .subscribe({
+              next: (value) => monster = value
+            });
 
-        if (!monster) {
-          console.warn(`Monster ${id} not found in store`);
-          return;
+          if (!monster) {
+            console.warn(`Monster ${id} not found in store`);
+            return;
+          }
+          this.monsterService.activateMonster$.next(monster);
         }
-        this.monsterService.activateMonster$.next(monster);
-      }
-    });
+      });
   }
 
   constructor(
@@ -66,6 +70,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.appService.scenarioStore.level$.pipe(takeUntil(this._destroy$))
+      .subscribe({
+        next: (value) => {
+          this.levelInputControl.setValue(value.toString());
+        }
+      })
   }
 }
