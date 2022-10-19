5f07b88175297ac26eae9da57634870f0ad2937d
Kyle McKnight <kamcknig@gmail.com>
2022-10-19 15:59:06 -0400

    added view mode 'v' to alow changing to list mode via the query params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { MonsterService } from './monster/services/monster.service';
 import { AppService } from './app.service';
 import { CombatService } from './combat/services/combat.service';
 import { combineLatest, Observable, Subject } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import {filter, map, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'gloomhaven-helper';
   public viewMode = 'normal';
   private _roundComplete$ = new Subject();
@@ -45,14 +46,25 @@ export class AppComponent {
   constructor(
     public monsterService: MonsterService,
     public appService: AppService,
-    public combatService: CombatService
+    public combatService: CombatService,
+    private _route: ActivatedRoute
   ) {
+  }
+
+  public ngOnInit(): void {
+    combineLatest([
+      this._route.queryParams.pipe(map(r => r['v']), filter(r => !!r)),
+      this.appService.scenarioStore.viewMode$
+    ]).subscribe({
+      next: ([paramViewMode, currentViewMode]) => paramViewMode && currentViewMode !== paramViewMode ? this.appService.toggleViewMode$.next() : null
+    });
+
     this._roundComplete$.pipe(
-      switchMap(() => combatService.store.activeMonsters$),
+      switchMap(() => this.combatService.store.activeMonsters$),
       filter(monsters => !!Object.keys(monsters)?.length)
     )
       .subscribe({
-        next: () => combatService.roundComplete$.next()
+        next: () => this.combatService.roundComplete$.next()
       });
 
     this.sortedMonsters$ = combineLatest([
